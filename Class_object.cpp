e6266b8d415fd2a8863e091d54e79027db7d74d6
hasee-codes <haseenasoultane2008@gmail.com>
2026-09-04 12:39:32 +0530

    Add Student class with display method

---
+++ Class_object.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+public:
+    string name;
+    int age;
+
+    void display()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
+};
+
+int main()
+{
+    Student s;
+
+    s.name = "Arun";
+    s.age = 20;
+
+    s.display();
+
+    return 0;
+}
