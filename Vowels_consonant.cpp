ab66a7ccdae8b47b2a4990827b7a3c071ba1af5a
hasee-codes <haseenasoultane2008@gmail.com>
2026-09-04 11:55:00 +0530

    Add Vowels and Consonants counting program

---
+++ Vowels_consonant.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main()
+{
+    string str;
+    int vowels = 0, consonants = 0;
+
+    cout << "Enter a string: ";
+    cin >> str;
+
+    for (char ch : str)
+    {
+        ch = tolower(ch);
+
+        if (ch >= 'a' && ch <= 'z')
+        {
+            if (ch == 'a' || ch == 'e' || ch == 'i' ||
+                ch == 'o' || ch == 'u')
+                vowels++;
+            else
+                consonants++;
+        }
+    }
+
+    cout << "Vowels = " << vowels << endl;
+    cout << "Consonants = " << consonants;
+
+    return 0;
+}
