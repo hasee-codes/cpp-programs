211fe65c517abb96d984179bdbdc55830cb52d2a
hasee-codes <haseenasoultane2008@gmail.com>
2026-09-04 12:40:23 +0530

    Create Constructor.cpp

---
+++ Constructor.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+public:
+    string name;
+    int age;
+
+    Student()
+    {
+        name = "Arun";
+        age = 20;
+    }
+
+    void display()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Age: " << age << endl;
+    }
+};
+
+int main()
+{
+    Student s;
+
+    s.display();
+
+    return 0;
+}
