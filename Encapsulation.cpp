eeda25e3438bfe49c7075bef04644fabcb17ee45
hasee-codes <haseenasoultane2008@gmail.com>
2026-09-12 21:16:22 +0530

    Add Student class with encapsulation for marks

---
+++ Encapsulation.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+private:
+    int marks;
+
+public:
+    void setMarks(int m)
+    {
+        marks = m;
+    }
+
+    int getMarks()
+    {
+        return marks;
+    }
+};
+
+int main()
+{
+    Student s;
+
+    s.setMarks(85);
+
+    cout << "Student Marks = " << s.getMarks();
+
+    return 0;
+}
